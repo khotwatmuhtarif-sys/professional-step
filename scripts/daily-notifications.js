f161b2216ded24c9a13529f984e109d120c8967e
khotwatmuhtarif-sys <khotwat.muhtarif@gmail.com>
2026-09-05 18:38:56 +0300

    Update daily-notifications.js

--- scripts/daily-notifications.js
+++ scripts/daily-notifications.js
@@ -248,7 +248,11 @@ async function main() {
   console.log(`Done. Sent ${sent} notification(s).`);
 }
 
-main().catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
